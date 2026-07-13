bd6973f8baeaccbe084d15d43d4b8350bc8a4820
endrisusanto <endrisusantosekarang@gmail.com>
2026-07-13 08:31:29 +0700

    feat: add flutter app, kotlin widget, and release workflow

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:flutter/material';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:home_widget/home_widget.dart';
 
